4bc78245ae3d52f69fefd40d7fdb3a9ba982698b
Alejandro Cruz <hcruz0716@gmail.com>
2022-11-10 01:03:15 -0600

    Drag 'n' drop

--- src/pages/dashboard/updates.jsx
+++ src/pages/dashboard/updates.jsx
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDropzone } from "react-dropzone";
 
 export function Updates() {
-  return (
-    <div className="mt-12">
-      <div className="mb-4 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4">
-        <h1>updates</h1>
+  const [files, setFiles] = useState([]);
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: {
+      "image/*": [],
+    },
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
+    },
+  });
+
+  const thumbs = files.map((file) => (
+    <div key={file.name}>
+      <div className="flex min-w-0 overflow-hidden">
+        <img
+          src={file.preview}
+          className="black h-full w-auto"
+          // Revoke data uri after image is loaded
+          onLoad={() => {
+            URL.revokeObjectURL(file.preview);
+          }}
+        />
       </div>
     </div>
+  ));
+
+  useEffect(() => {
+    // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
+    return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
+  }, []);
+
+  return (
+    <section className="container h-3/4 w-full">
+      <div {...getRootProps({ className: "dropzone" })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+      <aside className="flex flex-row flex-wrap pt-6">{thumbs}</aside>
+    </section>
   );
 }
 
